7a00a9325b006af1ba055df042077f9b8107e621
iain nash <me@iain.in>
2026-01-14 16:47:16 -0500

    cleanup stars

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,8 +1,7 @@
 use nannou::prelude::*;
-use nannou::{
-    geom::{pt2, Point2},
-    math::deg_to_rad,
-};
+
+mod star;
+use star::Star;
 
 fn main() {
     nannou::app(model).update(update).simple_window(view).run();
@@ -14,53 +13,15 @@ fn model(_app: &App) -> Model {
     Model {}
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {}
-
-pub struct Star {
-    width: f32,
-    height: f32,
-}
-
-impl Star {
-    pub fn new(width: f32, height: f32) -> Self {
-        Self { width, height }
-    }
-    fn move_by_quadrant(&self, x_offset: f32, y_offset: f32, points: Vec<Point2>) -> Vec<Point2> {
-        points
-            .iter()
-            .map(|p| pt2(p.x + x_offset * self.width, p.y + y_offset * self.height))
-            .collect()
-    }
-    fn partial_circle_points(&self, start_angle: i32, end_angle: i32) -> Vec<Point2> {
-        (start_angle..end_angle)
-            .map(|angle| {
-                let rad = deg_to_rad(angle as f32);
-                let x = self.width * rad.cos();
-                let y = self.height * rad.sin();
-                pt2(x, y)
-            })
-            .collect()
-    }
-    pub fn points(&self) -> Vec<Point2> {
-        vec![
-            self.move_by_quadrant(1., -1., self.partial_circle_points(90, 180)),
-            self.move_by_quadrant(-1., -1., self.partial_circle_points(0, 90)),
-            self.move_by_quadrant(-1., 1., self.partial_circle_points(270, 360)),
-            self.move_by_quadrant(1., 1., self.partial_circle_points(180, 270)),
-        ]
-        .into_iter()
-        .flatten()
-        .collect()
-    }
-}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
 
-    for point in Star::new(200., 200.).points().iter().step_by(10) {
+    for point in star::Star::new(200., 200.).points().iter().step_by(10) {
         let star = Star::new(
-            (4. + point.y.abs() / (app.time.sin().abs() + 0.55)),
+            4. + point.y.abs() / (app.time.sin().abs() + 0.55),
             6. + 10. * (1. - app.time.sin()).abs(),
         );
         draw.polygon()
